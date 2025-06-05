712491c21b53bf4b883e90c8e3948f63c2576330
ara fa adri <arafa.sco@gmail.com>
2025-06-05 10:31:30 +0700

    build: make estate modul

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Onesinus Tamba",
+    'category': 'App',
+    'description': """
+        This module is used to learn basic odoo 17 technical
+    """,
+    'application': True,
+    'data': [
+        # Security
+        'security/ir.model.access.csv',
+
+        # templates
+        # 'data/templates/example_email_template.xml',
+
+        # views
+        'views/estate_property.xml',
+        'views/estate_property_type.xml',
+        'views/estate_property_offer.xml',
+        'views/estate_property_tag.xml',
+        'views/menu.xml', # the order position matter!
+
+        # Load initial Data 
+        'data/estate.property.csv',
+
+        # Schedulers
+        'views/schedulers/estate_property_scheduler.xml'
+
+        #
+    ],
+}
