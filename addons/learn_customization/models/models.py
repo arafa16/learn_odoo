0abcb81c779e53dd020c305b67dda0e10c49ead6
ara fa adri <arafa.sco@gmail.com>
2025-06-05 10:32:49 +0700

    build: make customization modul

---
+++ addons/learn_customization/models/models.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class EstatePropertyTagInherit(models.Model):
+    _inherit = "estate.property.tag"
+
+    priority = fields.Integer(string="Tag Priority", default=0)
+    color = fields.Integer(string="Color Index Overide field", required=True)
+
+    # def create(self):
+    # 	# put your codes here
+    #     return super().create()
+
+    class SalesOrderInherit(models.Model):
+        _inherit = "sale.order"
+
+        client_document_number = fields.Char(string="SO Client Number")
